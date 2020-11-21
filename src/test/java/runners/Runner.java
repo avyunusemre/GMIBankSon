a845c50b7a01d4321bc5cd9712f31d60af5624df
hasanbasri0 <hasanbasriuysal3@gmail.com>
2020-11-21 19:42:40 +0200

    login

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         //plugin = "html:target\\default-cucumber-reports",//for windows
         features ="src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@db",
+        tags = "@US_10",
         dryRun = false
 )
 public class Runner {
