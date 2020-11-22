39a238572020b63854d2c917d6bfdb2015d9abc5
Mehmet <haciarifyildiz@gmail.com>
2020-11-22 10:27:55 +0100

    locater

---
+++ src/test/java/pages/GMICreatPassword.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class GMICreatePassword {
+    public GMICreatePassword(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    }
