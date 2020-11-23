4d23371a7c78a42fb4958679ac684ca8f4e10c9b
Mustafa <mustafayildirim8845@gmail.com>
2020-11-23 16:41:18 +0100

    US09 StepDefinitions and Feature

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         //plugin = "html:target\\default-cucumber-reports",//for windows
         features ="src/test/resources/features",
         glue = "stepdefinitions",
-        tags = " @US15TCs",
+        tags = "@USUserPageNewCustomer_TC002",
         dryRun = false
 )
 public class Runner{
