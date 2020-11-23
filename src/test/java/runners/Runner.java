99e6888775988ada7f071fbd268521cac484403d
Mustafa <mustafayildirim8845@gmail.com>
2020-11-23 16:48:28 +0100

    new tags

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         //plugin = "html:target\\default-cucumber-reports",//for windows
         features ="src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@USUserPageNewCustomer_TC002",
+        tags = "@US15TCs",
         dryRun = false
 )
 public class Runner{
