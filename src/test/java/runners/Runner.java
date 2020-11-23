3adc7aa89da75c928250ab0dd6956cfc372eca27
Huseyn <36949730+huseynshahinduran@users.noreply.github.com>
2020-11-23 12:07:44 +0000

    US12-13

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         //plugin = "html:target\\default-cucumber-reports",//for windows
         features ="src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@US06_TC_01",
+        tags = "@US12",
         dryRun = false
 )
 public class Runner {
