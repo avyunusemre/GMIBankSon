2ea4707f8534bafede2a410bbc58c92cb27a0394
Huseyn <36949730+huseynshahinduran@users.noreply.github.com>
2020-12-14 21:53:35 +0000

    Last Update

--- src/test/java/stepdefinitions/US26_Huseyn_StepDefinitions.java
+++ src/test/java/stepdefinitions/US26_Huseyn_StepDefinitions.java
@@ -1,5 +1,6 @@
 package stepdefinitions;
 
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
@@ -13,12 +14,19 @@ import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 
-public class US26_Huseyn_StepDefinitons {
+public class US26_Huseyn_StepDefinitions {
 
     Response response;
+    ObjectMapper obj=new ObjectMapper();
+    Country[] countries;
+    Country countryUpdated;
+    Response responseUpdated;
+    Response response2;
+    int firstCountryId;
+    String updatedCountry="Scotland";
 
     @Given("Let's see all data on the screen using GET {string}")
-    public void let_s_see_all_data_on_the_screen_using_GET(String endpoint) {
+    public void let_s_see_all_data_on_the_screen_using_GET(String string) {
 
         response= given().
                 contentType(ContentType.JSON).
@@ -26,64 +34,76 @@ public class US26_Huseyn_StepDefinitons {
                 oauth2(ConfigurationReader.
                         getProperty("token")).
                 when().
-                get(endpoint);
-        response.prettyPrint();
+                get(string);
+
+//        response.prettyPrint();
 
     }
 
     @Given("HTTP Status Code {int}")
     public void http_Status_Code(Integer code) {
+
         response.
                 then().
                 assertThat().
                 statusCode(code);
-    }
 
-
-    @Given("Let's update the country {string} using PUT endpoint {string} id {int}")
-    public void let_s_update_the_country_using_PUT_endpoint_id(String country, String url, Integer id) {
-
-        String update= "{\"id\":id,\"name\": Belgium\"\"}";
-
-        response=given().
-                contentType(ContentType.JSON).
-                auth().
-                oauth2(ConfigurationReader.
-                        getProperty("token")).
-                body(update).
-                when().
-                put(country);
-        response.prettyPrint();
-    }
-
-    @Given("After creating with PUT, let's verify the country created {string}")
-    public void after_creating_with_PUT_let_s_verify_the_country_created(String string) throws IOException {
-        ObjectMapper objectMapper=new ObjectMapper();
-        Country country=objectMapper.readValue(response.asString(),Country.class);
-
-        String actual=country.getName();
-
-        Assert.assertEquals(actual,string);
-        System.out.println("Actual: "+actual);
-        System.out.println("Expected: "+string);
-    }
-
-    @Given("Let's update the country {string} using PATCH endpoint {string} id {int}")
-    public void let_s_update_the_country_using_PATCH_endpoint_id(String string, String string2, Integer int1) {
 
     }
 
-    @Given("After creating with PATCH, let's verify the country created {string}")
-    public void after_creating_with_PATCH_let_s_verify_the_country_created(String string) throws IOException {
-        ObjectMapper objectMapper=new ObjectMapper();
-        Country country=objectMapper.readValue(response.asString(),Country.class);
+    @Given("Get all countries as De-Serialization")
+    public void get_all_countries_as_De_Serialization() throws Exception {
 
-        String actual=country.getName();
+        countries = obj.readValue(response.asString(), Country[].class);
 
-        Assert.assertEquals(actual,string);
-        System.out.println("Actual: "+actual);
-        System.out.println("Expected: "+string);
     }
 
+    @Given("Let's update the first ID country using PUT endpoint {string}")
+    public void let_s_update_the_first_ID_country_using_PUT_endpoint(String endpoint) throws Exception {
+
+        firstCountryId = countries[0].getId();
+
+        response2 = given()
+                .headers(
+                        "Authorization",
+                        "Bearer " + ConfigurationReader.getProperty("token"),
+                        "Content-Type",
+                        ContentType.JSON,
+                        "Accept",
+                        ContentType.JSON)
+                .when()
+                .get(endpoint+firstCountryId);
+        response2.prettyPrint();
+
+
+        System.out.println(firstCountryId);
+
+        countryUpdated = new Country(firstCountryId,updatedCountry,null);
+        String updated = obj.writeValueAsString(countryUpdated);
+
+        responseUpdated = given()
+                .headers(
+                        "Authorization",
+                        "Bearer " + ConfigurationReader.getProperty("token"),
+                        "Content-Type",
+                        ContentType.JSON,
+                        "Accept",
+                        ContentType.JSON)
+                .body(updated)
+                .when()
+                .put(endpoint);
+        responseUpdated.prettyPrint();
+        System.out.println(responseUpdated.getStatusCode());
+
+    }
+
+    @Then("After creating with PUT, let's verify the country created")
+    public void after_creating_with_PUT_let_s_verify_the_country_created() {
+
+        String actual=responseUpdated.path("name");
+
+        Assert.assertEquals(updatedCountry, actual);
+
+    }
 
 }
